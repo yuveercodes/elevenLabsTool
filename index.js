8bbbb14227faa691af841f23b105712499c2803c
Yuveer Mulchandani <103843434+yuveercodes@users.noreply.github.com>
2025-03-22 20:04:44 +0530

    node-fetch implementation

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express=require("express")
-const https = require("follow-redirects").https;
+
 const app = express();
 
-app.get("/:num", (req,res) => {
-    var finalData = {};
-    https.get(`https://script.google.com/macros/s/AKfycbxuIoJMc79TD6c8h7Nbmrif-8PPTLkMKtf1rMatHVYnQ7tod2uI5YF8kGNW6b3ac_Aa/exec?path=Orders&action=readOne&num=${req.params.num}` , resp => {
-        let data ="";
-        resp.on('data', chunk => data+=chunk);
-        resp.on('end', () => {
-            finalData = JSON.parse(data);
-            res.send(finalData);
-        })
-    })
-
-})
 
 app.listen(3000, () => console.log("running"))
--- netlify/functions/getPastOrders/getPastOrders.mjs
+++ netlify/functions/getPastOrders/getPastOrders.mjs
@@ -1,19 +1,20 @@
-import followRedirects from "follow-redirects";
-export default (request, context) => {
+const fetch = require('node-fetch'); // Make sure to install this package
+
+exports.handler = async function(event, context) {
+  const url = `https://script.google.com/macros/s/AKfycbxuIoJMc79TD6c8h7Nbmrif-8PPTLkMKtf1rMatHVYnQ7tod2uI5YF8kGNW6b3ac_Aa/exec?path=Orders&action=readOne&num=${event.queryStringParmameters.num}`; // Replace with the URL you want to fetch data from
+
   try {
-    const url = new URL(request.url)
-    const num = url.searchParams.get('num') || '9136641620'
-    followRedirects.https.get(`https://script.google.com/macros/s/AKfycbxuIoJMc79TD6c8h7Nbmrif-8PPTLkMKtf1rMatHVYnQ7tod2uI5YF8kGNW6b3ac_Aa/exec?path=Orders&action=readOne&num=${num}` , resp => {
-        let data ="";
-        resp.on('data', chunk => data+=chunk);
-        resp.on('end', () => {
-            return new Response(JSON.parse(data))
-        })
-    })
-    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
-    return new Response(error.toString(), {
-      status: 500,
-    })
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' }),
+    };
   }
-}
+};
